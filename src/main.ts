9a61541094862efe236eb30b037d383e80d754a0
faiz-dev <a.faizmail@gmail.com>
2026-02-24 22:57:10 +0700

    install swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformResponseInterceptor } from './shared/infrastructure/interceptors/transform-response.interceptor';
 import { AllExceptionsFilter } from './modules/user/infrastructure/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,16 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new TransformResponseInterceptor(new Reflector()));
+
+  const config = new DocumentBuilder()
+    .setTitle('LMS Backend API')
+    .setDescription('API documentation for the LMS platform')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
